0aea343a0e7674ffd8bfff5de9d5336bca00d03d
Anay Gondhalekar <31811411+AnayGondhalekar@users.noreply.github.com>
2017-10-04 23:51:34 -0600

    Main file

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <memory.h>
+#include <debug.h>
+#include <conversion.h>
+
+
+
+int main()
+{
+#ifdef PROJECT1
+project1();
+#endif
+project1();
+return 0;
+}
